5a85e8272ad98b72b94e85e12b3ee3e85587629f
Horino Yutaro <suzy.city16@gmail.com>
2021-02-02 00:57:46 +0900

    [add] validationの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,7 @@
 #  updated_at :datetime         not null
 #
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :title, length: { minimum: 2, maximum: 30 }
+  validates :content, presence: true
 end
